56e49a97a89e2bfe01b05e4af0d4b42d9f9d5630
Jimmy Shah <jimmyshah83@gmail.com>
2025-09-08 11:09:02 -0400

    Entra ID authentication

--- src/foundry_agents/main.py
+++ src/foundry_agents/main.py
@@ -1,20 +1,38 @@
 """
 Main class to manage the Foundry Agents.
 """
+import os
+import asyncio
+import dotenv
+from azure.ai.projects.aio import AIProjectClient
+from azure.identity.aio import DefaultAzureCredential
 
-
+dotenv.load_dotenv()
 
 class Main:
     """Simple main class that prints a greeting."""
 
-    def run(self) -> None:
+    _client: AIProjectClient
+    _credential: DefaultAzureCredential
+
+    def __init__(self) -> None:
+        self._credential = DefaultAzureCredential()
+        self._client = AIProjectClient(
+            credential=self._credential,
+            endpoint=os.getenv("AZURE_AI_PROJECTS_ENDPOINT"),
+        )
+        print("Initialized AIProjectClient.")
+
+    async def run(self) -> None:
         """Run the main logic."""
         print("hello world")
 
+def cli() -> None:
+    """CLI entry point for the package."""
+    async def main() -> None:
+        await Main().run()
+    
+    asyncio.run(main())
 
-def main() -> None:
-    """Entry point helper."""
-    Main().run()
-
-if __name__ == "__main__": 
-    main()
+if __name__ == "__main__":
+    cli()
